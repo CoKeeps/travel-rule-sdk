e46926741da15b4f7fc2d60c5f6f07b31d1306a1
syahirCoKeeps <syahiramali@cokeeps.com>
2026-01-14 08:27:49 +0800

    Migration to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
   treeshake: true,
   minify: false,
   external: ['axios', 'jose', 'uuid', 'zod'],
+  target: false,
 });
 
